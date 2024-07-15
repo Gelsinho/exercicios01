4569d6e510fff63c1a177e989922254a1aaf1510
Gelsinho <gelsonhenrique122007@gmail.com>
2024-07-15 16:54:56 -0300

    Create exerciocio2.py

---
+++ exerciocio2.py
@@ -0,0 +1,10 @@
+# Crie uma lista chamada números utilizando a função range() para gerar uma sequência de números de 1 a 10 (inclusivo).
+lista = list(range(11))
+print(lista)
+
+# Fatiando para obter pares (começa no segumdo índice, pula de 2 em 2)
+parte_da_lista = lista [1::2]
+print(parte_da_lista) 
+
+pares = list[1::2] # Mostrando a lista de números
+print(pares) # mostrando a lista de pares
