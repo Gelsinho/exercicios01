70e44a17a46521c3fdb77ea8e08809cb2b4e8889
Gelsinho <gelsonhenrique122007@gmail.com>
2024-07-15 16:54:07 -0300

    Create exercio1.py

---
+++ exercio1.py
@@ -0,0 +1,18 @@
+# cria uma lista chamada mylista
+
+mylista = ["iphone", "cachorro", "caramelo", "palmeiras"]
+
+# acessando o segundo elemento (cachorro)
+print(mylista[1])
+
+# adiciione um item na minha lista  
+mylista.append("manga")
+
+# mostramdo a quantidade de elementos (5)
+print(len(mylista[5]))
+
+# remover um item da minha lista (iphone)
+mylista.remove("iphone")
+
+# Exibe o resultado final
+print(mylista)  
